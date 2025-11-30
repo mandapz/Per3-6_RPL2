5030783676333cf31ee5b931021f125db132373c
mandapz <amandaaprilianurafifah04@gmail.com>
2025-11-30 17:53:16 +0700

    Initial commit

---
+++ Pert3_50422188/src/main/java/com/mycompany/pert3_50422188/Pert3_50422188.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert3_50422188;
+
+/**
+ *
+ * @author naufa
+ */
+public class Pert3_50422188 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Pert5_50422188/src/main/java/com/mycompany/pert5_50422188/repository/MahasiswaRepository.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pert5_50422188.repository;
+
+import com.mycompany.pert5_50422188.model.ModelMahasiswa;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ *
+ * @author naufa
+ */
+@Repository
+public interface MahasiswaRepository extends JpaRepository<ModelMahasiswa, Long> {
+
+    public List<ModelMahasiswa> findAll();
+
+}
